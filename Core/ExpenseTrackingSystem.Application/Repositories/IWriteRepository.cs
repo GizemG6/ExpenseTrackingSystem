48b46a93841ebc8869d4232a7b321a0ceda07ab8
cang8 <0.gizemgunes@gmail.com>
2025-04-20 21:06:07 +0300

    added-IWriteRepository

---
+++ Core/ExpenseTrackingSystem.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTrackingSystem.Application.Repositories
+{
+    public interface IWriteRepository<T, Tkey> where T : class
+	{
+		Task<bool> AddAsync(T entity);
+		Task<bool> AddRangeAsync(List<T> entities);
+		Task<bool> RemoveAsync(Tkey id);
+		Task<bool> RemoveRangeAsync(List<Tkey> ids);
+		Task<bool> UpdateAsync(T entity);
+		Task<bool> SaveChangesAsync();
+	}
+}
