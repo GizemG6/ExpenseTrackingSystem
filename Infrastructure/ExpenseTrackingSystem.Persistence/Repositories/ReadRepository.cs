918b6854ea258374b4e6cad77504cf045836fc0b
cang8 <0.gizemgunes@gmail.com>
2025-04-20 21:43:03 +0300

    added-Read-and-WriteRepository-to-Persistence

---
+++ Infrastructure/ExpenseTrackingSystem.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,64 @@
+﻿using ExpenseTrackingSystem.Application.Repositories;
+using ExpenseTrackingSystem.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTrackingSystem.Persistence.Repositories
+{
+	public class ReadRepository<T, TKey> : IReadRepository<T, TKey> where T : class
+	{
+		private readonly AppDbContext _context;
+
+		public ReadRepository(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public DbSet<T> Table => _context.Set<T>();
+
+		public async Task<List<T>> GetAllAsync(bool tracking = true)
+		{
+			var query = Table.AsQueryable();
+			if (!tracking)
+				query = query.AsNoTracking();
+
+			return await query.ToListAsync();
+		}
+
+		public async Task<T> GetByIdAsync(TKey id, bool tracking = true)
+		{
+			var entity = await Table.FindAsync(id);
+
+			if (entity == null)
+				return null;
+
+			if (!tracking)
+				_context.Entry(entity).State = EntityState.Detached;
+
+			return entity;
+		}
+
+		public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true)
+		{
+			var query = Table.AsQueryable();
+			if (!tracking)
+				query = query.AsNoTracking();
+
+			return await query.FirstOrDefaultAsync(method);
+		}
+
+		public IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true)
+		{
+			var query = Table.Where(method);
+			if (!tracking)
+				query = query.AsNoTracking();
+
+			return query;
+		}
+	}
+}
---
+++ Infrastructure/ExpenseTrackingSystem.Persistence/Repositories/WriteRepository.cs
@@ -0,0 +1,66 @@
+﻿using ExpenseTrackingSystem.Application.Repositories;
+using ExpenseTrackingSystem.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTrackingSystem.Persistence.Repositories
+{
+	public class WriteRepository<T, TKey> : IWriteRepository<T, TKey> where T : class
+	{
+		private readonly AppDbContext _context;
+
+		public WriteRepository(AppDbContext context)
+		{
+			_context = context;
+		}
+		public DbSet<T> Table => _context.Set<T>();
+
+		public async Task<bool> AddAsync(T entity)
+		{
+			await Table.AddAsync(entity);
+			return await SaveChangesAsync();
+		}
+
+		public async Task<bool> AddRangeAsync(List<T> entities)
+		{
+			await Table.AddRangeAsync(entities);
+			return await SaveChangesAsync();
+		}
+
+		public async Task<bool> RemoveAsync(TKey id)
+		{
+			var entity = await Table.FindAsync(id);
+			if (entity == null)
+				return false;
+
+			Table.Remove(entity);
+			return await SaveChangesAsync();
+		}
+
+		public async Task<bool> RemoveRangeAsync(List<TKey> ids)
+		{
+			var entities = await Table.Where(e => ids.Contains(EF.Property<TKey>(e, "Id"))).ToListAsync();
+			if (entities.Any())
+			{
+				Table.RemoveRange(entities);
+			}
+			return await SaveChangesAsync();
+		}
+
+		public async Task<bool> UpdateAsync(T entity)
+		{
+			Table.Update(entity);
+			return await SaveChangesAsync();
+		}
+
+		public async Task<bool> SaveChangesAsync()
+		{
+			return await _context.SaveChangesAsync() > 0;
+		}
+	}
+}
