80f83dc6a2015d84f281f8785366289a65842a53
cang8 <0.gizemgunes@gmail.com>
2025-04-20 21:03:09 +0300

    added-IReadRepository-and-changed-AuditLogId

---
+++ Core/ExpenseTrackingSystem.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTrackingSystem.Application.Repositories
+{
+	public interface IReadRepository<T, TKey> where T : class
+	{
+		Task<List<T>> GetAllAsync(bool tracking = true);
+		IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true);
+		Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true);
+		Task<T> GetByIdAsync(TKey id, bool tracking = true);
+		DbSet<T> Table { get; }
+	}
+}
--- Core/ExpenseTrackingSystem.Domain/Entities/AuditLog.cs
+++ Core/ExpenseTrackingSystem.Domain/Entities/AuditLog.cs
@@ -8,7 +8,7 @@ namespace ExpenseTrackingSystem.Domain.Entities
 {
 	public class AuditLog
 	{
-		public int Id { get; set; }
+		public Guid Id { get; set; }
 		public string UserId { get; set; }
 		public string Action { get; set; }
 		public string Entity { get; set; }
